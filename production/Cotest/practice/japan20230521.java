eaa1ffa78932f48c947dd584a1abce10edeb95c3
dongwoo2 <ieiedong@naver.com>
2023-05-21 22:22:17 +0900

    20230521japan

---
+++ production/Cotest/practice/japan20230521.java
@@ -0,0 +1,15 @@
+package production.Cotest.practice;
+
+class best{
+    public String travel(){
+        return "numberonejapantravel";
+    }
+    
+}
+public class japan20230521 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
