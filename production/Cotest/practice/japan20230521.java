9bc07175d3718a3852c6ea16841a93038dd2b19e
dongwoo2 <ieiedong@naver.com>
2023-05-22 10:49:38 +0900

    함수호출20230522

--- production/Cotest/practice/japan20230521.java
+++ production/Cotest/practice/japan20230521.java
@@ -1,15 +1,38 @@
 package production.Cotest.practice;
 
+import java.util.Scanner;
+
 class best{
     public String travel(){
         return "numberonejapantravel";
     }
-    
+
+    public int sub(int x, int y){
+        return x + y;
+    }
+
+    public int minus(int x, int y){
+        return x - y;
+    }
+
+    public int gob(int x, int y){
+        return x * y;
+    }
+
+    public int nanum(int x, int y){
+        return x/y;
+    }
 }
 public class japan20230521 {
 
     public static void main(String[] args) {
 
 
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+//        minus(a, b);
     }
 }
